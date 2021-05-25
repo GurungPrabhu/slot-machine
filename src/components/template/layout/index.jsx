3c6b43b0004312b8b7073d9ae9613a88b2245032
unknown <prabhu.eversoft@gmail.com>
2021-05-25 11:59:33 +0545

    INIT

---
+++ src/components/template/layout/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Layout = ({ children }) => <div>{children}</div>;
+export default Layout;
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
